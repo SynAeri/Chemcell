95e281ab31c197be7fdf4b0e5a2444c42e55534d
SynAeri <164128582+SynAeri@users.noreply.github.com>
2024-07-18 02:29:19 +1000

    Updated the new parser
    This functions to find the list of elements in the page while we parse through each element

--- Main_parser/new_parser.py
+++ Main_parser/new_parser.py
@@ -11,17 +11,14 @@ def get_html_tables(Name):
     #then when the webpage is shown where it outputs the links of different reactions
     #we will then list them in a table for them to iterate again and store these in an
     #external table 
-    url = str.format('https://webbook.nist.gov/cgi/cbook.cgi?React={0}&React2=&Prod=&Prod2=&Rev=on&AllowOtherReact=on&AllowOtherProd=on&Type=Any&Units=SI', Name.lower())
-
-    #gets the html
-    actualHtml = get_response(url)
-
-    #beautifulsoup to parse
-    html = BeautifulSoup(actualHtml, 'html.parser')
-
-    link_iter = html.find()
-
-
+    def response(resp):
+        #if it is a functioning HTML
+        content = resp.headers['Content-Type'].lower()
+        return (resp.status_code == 200
+                and content is not None
+                and content.find('html') > -1)
+    
+    
     def get_response(url):
         #return html to parse, return none if it cant reach page
         with closing(get(url, stream=True)) as resp:
@@ -29,11 +26,19 @@ def get_html_tables(Name):
                 return resp.content
             else:
                 return None
+            
+    url = str.format('https://webbook.nist.gov/cgi/cbook.cgi?React={0}&React2=&Prod=&Prod2=&Rev=on&AllowOtherReact=on&AllowOtherProd=on&Type=Any&Units=SI', Name)
+    #(He+ • helium) + helium = (He+ • 2helium)
+
+    #gets the html
+    actualHtml = get_response(url)
+
+    #beautifulsoup to parse
+    html = BeautifulSoup(actualHtml, 'html.parser')
+
+    link_iter = html.find(id="main")
+    element_lists = link_iter.find('ol')
     
-    def response(resp):
-        #if it is a functioning HTML
-        content = resp.headers['Content-Type'].lower()
-        return (resp.status_code == 200
-                and content is not None
-                and content.fine('html') > -1)
+    return(len(element_lists.find_all("li", {"class": "mixture"})))
     
+print(get_html_tables("Hcl"))
