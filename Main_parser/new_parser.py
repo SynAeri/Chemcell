85bfbfacfd4300a95f3acb4d415db840489b1422
SynAeri <164128582+SynAeri@users.noreply.github.com>
2024-07-19 06:43:44 +1000

    Update new_parser.py

--- Main_parser/new_parser.py
+++ Main_parser/new_parser.py
@@ -5,11 +5,27 @@ from urllib.request import urlopen
 from urllib.request import urlretrieve
 from contextlib import closing
 
+elements = [
+    "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+    "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
+    "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+    "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
+    "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+    "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
+    "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+    "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg",
+    "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
+    "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+    "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
+    "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"
+]
+
 def get_html_tables(Name):
     #the plan
     #to make it iterate the sites including the element
     #then when the webpage is shown where it outputs the links of different reactions
-    #we will then list them in a table for them to iterate again and store these in an
+    #we will then list them in a table in numpy df format and then we iterate those links
+    #those links we get the different names and stats of them
     #external table 
     def response(resp):
         #if it is a functioning HTML
@@ -36,8 +52,12 @@ def get_html_tables(Name):
     #beautifulsoup to parse
     html = BeautifulSoup(actualHtml, 'html.parser')
 
+    #we locate the segment where links are located
     link_iter = html.find(id="main")
     element_lists = link_iter.find('ol')
+    
+    #we now loop for the number of links that exist till each link has been appended to the table we are making
+    #The table we are now making is now hence checked thoroughly if they contain an organic SMILES image, if not then it is not an organic reaction and we remove it from the table
     
     return(len(element_lists.find_all("li", {"class": "mixture"})))
     
