fddab7903739377935534063f24394019cdf18d7
Jordan <164128582+SynAeri@users.noreply.github.com>
2024-09-12 14:07:29 +0000

    Edited the setup to work as intended

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 long_desc=""
 setup(
     name="chemcell",
-    version="0.01"
-    description="highly modifiable tabulation for chemical reactions"
+    version="0.01",
+    description="highly modifiable tabulation for chemical reactions",
     long_description= long_desc,
     author="Jordan Maquiran, Cooper Donnely",
     author_email="jordan.maquiran@outlook.com",
@@ -20,13 +20,13 @@ setup(
 
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Topic :: Documentation",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
-    ]
-    packages=find_packages()
+    ],
+    packages=find_packages(),
 )
