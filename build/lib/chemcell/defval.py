04ff3a5840575df93290fccaf811b9a1db1c5643
Jordan <164128582+SynAeri@users.noreply.github.com>
2024-09-12 17:11:51 +0000

    fixed extra

---
+++ build/lib/chemcell/defval.py
@@ -0,0 +1,20 @@
+DEFAULT_PUBCHEM_DATA = ['MolecularWeight', 
+                        'RotatableBondCount', 
+                        'DefinedBondStereoCount', 
+                        'Complexity', 
+                        'HBondDonorCount', 
+                        'HBondAcceptorCount', 
+                        'Complexity',
+                        'CovalentUnitCount',
+                        'Charge',
+                        'HeavyAtomCount',
+                        'IsotopeAtomCount'
+                        ]
+
+DEFAULT_CHEMEO_DATA = ['Electron affinity', 
+                        'Ionization energy', 
+                        'Critical Pressure', 
+                        'Dipole Moment',
+                        'Critical Temperature',
+                        'Octanol/Water partition coefficient'
+                        ]
